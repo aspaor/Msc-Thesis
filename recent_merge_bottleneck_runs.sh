279228f39a906a2427894df9b107d1884f074b65
Aspasia Orfanou <44154688+aspaor@users.noreply.github.com>
2021-06-22 12:08:11 +0300

    Add merge & bottleneck pipeline 1 run example

---
+++ recent_merge_bottleneck_runs.sh
@@ -0,0 +1,4 @@
+#merge p1~p2 and bottleneck in p1
+python3 wrapper_try.py bottleneck recent_merge 3 "100000,50000" "recent_merge=10000-90000-20000" "" "ppc:2000:500:0/ppc:10000-50000-10000:10000:0"
+
+
