2532f37735172bbc1f673f9c9d73ba45cf32faf5
Aspasia Orfanou <44154688+aspaor@users.noreply.github.com>
2021-06-22 12:11:49 +0300

    Add recombination pipeline 1 run examples

---
+++ recombination_runs.sh
@@ -0,0 +1,8 @@
+#recombination in 3 pops Scenario 1
+python3 wrapper_try.py recomb '' 3 "100000,50000" "" "" ""
+
+#recombination in 4 pops Scenario 7
+python3 wrapper_try.py recomb '' 4 "100000,50000,25000" "" "" ""
+
+
+
